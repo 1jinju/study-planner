1d52dd08468fa6251875ed151412c17ce4d728ea
1jinju <gkswlswn2044@gmail.com>
2023-06-09 16:58:55 +0900

    초기 설정

---
+++ src/test/java/kr/ac/jejunu/studyplanner/StudyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.studyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
